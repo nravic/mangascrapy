0703b232ae90d5e502062d62fce25e1942da5ae6
Niranjan Ravichandra <niranjan96dxb@gmail.com>
2016-07-30 07:34:10 +0000

    major fixes to issue with the run file not picking up settings. Can now run simply by invoking python3 mangascrapy.py. Also fixed issues with zipping and removal of temp files. As of today, runs perfectly with mangastream chapters.

--- mangascrapy.py
+++ mangascrapy.py
@@ -6,6 +6,7 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from mangascraper.settings import *
 from shutil import make_archive
+import subprocess
 
 input_url = str(input('Enter page 1 of mangastream url: '))
 manga_info = input('Enter comma seperated values for manga and chapter: ')
@@ -23,5 +24,8 @@ if __name__ == "__main__":
     process.crawl(MangaStreamSpider)
     process.start() 
 
-make_archive('/home/scire/to-read/'+manga_info[0]+'_'+manga_info[1], 'zip', root_dir='/home/scire/to-read', base_dir='download')
+make_archive('/home/scire/to-read/'+manga_info[0]+'_'+manga_info[1], 'zip', root_dir='/home/scire/to-read', base_dir='downloads')
+
+subprocess.Popen(["rm", "-r", "/home/scire/to-read/downloads/"], stdout=subprocess.PIPE)
+
 
